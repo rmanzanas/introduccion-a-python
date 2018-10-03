2bab9da8bf91c50e11c460489a91eb3beb48bbbc
R. Manzanas <rmanzanas@ifca.unican.es>
2018-10-03 17:58:15 +0000

    ---

---
+++ lyrics/frequencies.py
@@ -0,0 +1,50 @@
+def words_to_frequencies(lyrics):
+    """
+    Convert words into frequencies. Return a dictionarky whose keys are the
+    words with the frequency as the value
+    """
+    freqs = {}
+    for word in lyrics:
+        if word in freqs:
+            freqs[word] += 1
+        else:
+            freqs[word] = 1
+        # Alternativa al if anterior
+        # freqs[word] = freqs.get(word, 0) + 1
+        # Otra alternativa
+        # freqs.setdefault(word, 0)
+        # freqs[word] += 1
+    return freqs
+
+
+def most_common_words(frequencies):
+    """
+    Return a tuple containing:
+    * The number of occurences of a word in the first tuple element
+    * A list containing the words with that frequency
+    """
+    value = frequencies.values()
+    maxval = max(value)
+    
+    words = []
+    for word, score in frequencies.items():
+        if score == max(value):
+            words.append(word)
+    return (maxval, words)
+
+
+def get_more_often_user_words(frequencies, threshold=10):
+    """
+    Return a list of the words that are used more often, above
+    the *optional* threshold. If no threshold is passed, use 10.
+    """
+    result = []
+    while True:
+        score = most_common_words(frequencies)
+        if score[0] < threshold:
+            break
+        for w in score[1]:
+            del frequencies[w]
+        result.append(score)
+        
+    return result
